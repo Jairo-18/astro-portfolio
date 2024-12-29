c35c2ad3b696180d184825c4d5c7887ec54da6c3
Jairo-18 <jhonlegarda1.2@gmail.com>
2024-12-29 01:40:34 -0500

    Fixed: css all page, darkmode ajust

--- src/data/Skills.ts
+++ src/data/Skills.ts
@@ -5,13 +5,14 @@ export interface Skills {
 
 export const skills: Skills[] = [
   { name: 'HTML5', category: 'frontend' },
-  { name: 'CSS3', category: 'frontend' },
+  { name: 'CSS', category: 'frontend' },
   { name: 'JavaScript', category: 'languages' },
   { name: 'TypeScript', category: 'languages' },
-  { name: 'AngularJS', category: 'frontend' },
+  { name: 'Angular', category: 'frontend' },
   { name: 'Astro', category: 'frontend' },
-  { name: 'Tailwind CSS', category: 'frontend' },
+  { name: 'Tailwind', category: 'frontend' },
   { name: 'React', category: 'frontend' },
   { name: 'Bootstrap', category: 'frontend' },
   { name: 'Git', category: 'tools' },
+  { name: 'Node', category: 'tools' },
 ];
